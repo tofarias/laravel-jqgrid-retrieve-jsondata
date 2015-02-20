746e94bab690ce623e091c1747d863c6e39d8974
Tiago Oliveira de Farias <tiago.farias.poa@gmail.com>
2015-02-20 08:46:04 -0200

    Create BillController.php

---
+++ src/BillController.php
@@ -0,0 +1,28 @@
+class BillController extends \BaseController implements IGrid{
+
+	private $fcfoRepo;
+	private $contractRepo;
+	private $orderRepo;
+	private $tmovRepo;
+	
+	public function __construct(IFCFO $fcfo, IContract $contract, IOrder $order, ITMOV $tmov)
+	{
+		parent::__construct();
+		
+		$this->fcfoRepo = $fcfo;
+		$this->contractRepo = $contract;
+		$this->orderRepo = $order;
+		$this->tmovRepo = $tmov;
+	}
+
+	public function getFetchData()
+	{
+		$contract = $this->contractRepo->findOrFail( \Input::get('contract_id') );
+		$billsBuilder = $this->tmovRepo->findAll()->whereCostCenter( $contract->cost_center );
+		
+		$billGrid = new BillGrid( $billsBuilder,TMov::fieldsInSelect(), \Input::get('rows'), \Input::get('page'), \Input::get('sidx'), \Input::get('sord') );
+		$data = $billGrid->buildRows()->getData();
+		
+		return \Response::json( $data );
+	}
+}
